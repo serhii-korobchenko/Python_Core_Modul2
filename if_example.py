45cf5786d6d1d11ee639c024ce33acf5ed6cc70a
Serhii Korobchenko <jchild2008@gmail.com>
2022-06-08 11:52:03 +0300

    Update if_example.py
    08062022

--- if_example.py
+++ if_example.py
@@ -45,8 +45,53 @@ if name and age >= 18 and has_driver_licence:
 is_nice = True
 state = "nice" if is_nice else "not nice"
 print(state)
-"""
+
 
 some_data = ""
 msg = some_data or "Не было возвращено данных"
 print(msg)
+
+
+### Блоки инструкций
+x = int(input("X: "))
+y = int(input("Y: "))
+
+if x == 0:
+    print("X can`t be equal to zero")
+    x = int(input("X: "))
+
+result = y / x
+print(result)
+
+
+
+x = int(input("X: "))
+y = int(input("Y: "))
+
+if x == 0:
+    print("X can`t be equal to zero")
+    x = int(input("X: "))
+
+    if x == 0:
+        print("X can`t be equal to zero")
+        x = int(input("X: "))
+
+        if x == 0:
+            print("X can`t be equal to zero")
+            x = int(input("X: "))
+
+result = y / x
+print(result)
+"""
+
+
+if x >= 0:
+    if y >= 0:               # x > 0, y > 0
+        print("Первая четверть")
+    else:                    # x > 0, y < 0
+        print("Четвертая четверть")
+else:
+    if y >= 0:               # x < 0, y > 0
+        print("Вторая четверть")
+    else:                    # x < 0, y < 0
+        print("Третья четверть")
